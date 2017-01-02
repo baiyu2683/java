8931567008062425caa9c7bcff63bb5b14a999ba
zhangheng2683 <zhangheng2683@gmail.com>
2017-01-02 13:26:48 +0800

    threadlocal例子

---
+++ src/main/java/com/zh/concurrent/ThreadLocalVariableHolder.java
@@ -0,0 +1,54 @@
+package com.zh.concurrent;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by zh on 2017-01-02.
+ */
+public class ThreadLocalVariableHolder {
+    public static ThreadLocal<Integer> value = new ThreadLocal<Integer>() {
+        private Random rand = new Random(47);
+        protected synchronized Integer initialValue() {
+            return rand.nextInt(10000);
+        }
+    };
+    public static void increment() {
+        value.set(value.get() + 1);
+    }
+    public static int get() {
+        return value.get();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0; i < 1; i++)
+            exec.execute(new Accessor(i));
+        TimeUnit.SECONDS.sleep(3);
+        exec.shutdownNow();
+    }
+}
+
+class Accessor implements Runnable {
+    private final int id;
+
+    public Accessor(int idn) {
+        id = idn;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            ThreadLocalVariableHolder.increment();
+            ThreadLocal t = ThreadLocalVariableHolder.value;
+            System.out.println(this);
+            Thread.yield();
+        }
+    }
+
+    public String toString() {
+        return "#" + id + ": " + ThreadLocalVariableHolder.get();
+    }
+}
