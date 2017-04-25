6a7c808e966aa7312f6ce45159e6a2fd1ce5cfea
zhangheng2683 <zhangheng2683@gmail.com>
2017-04-25 23:23:25 +0800

    测试

--- src/test/java/edition8/Java8Tester.java
+++ src/test/java/edition8/Java8Tester.java
@@ -4,10 +4,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by zhangheng on 2017/1/11.
@@ -45,5 +43,20 @@ public class Java8Tester {
         Collections.sort(list, (s1, s2) -> {
             return s2.compareTo(s1);
         });
+    }
+
+    @Test
+    public void testOptional() {
+        Optional<String> s = Optional.ofNullable(null);
+        System.out.println(s.orElse("123"));
+        System.out.println(s.orElseGet(() -> String.valueOf(new Random(47).nextInt())));
+//        System.out.println(s.orElseThrow());
+        String[] arr = new String[]{"1", "43", "23", "1", "74"};
+        System.out.println(Arrays.stream(arr).distinct().collect(Collectors.toList()));
+        int[] ints = new int[]{1,2,3,4,5,6,123,34,213,45};
+        IntSummaryStatistics intSummaryStatistics = Arrays.stream(ints).summaryStatistics();
+        System.out.println(intSummaryStatistics.getAverage());
+        System.out.println(intSummaryStatistics.getCount());
+        System.out.println(intSummaryStatistics.getMax());
     }
 }
