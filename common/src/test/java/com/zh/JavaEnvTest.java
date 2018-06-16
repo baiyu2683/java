0cd78eebd6eb474f5c59bc1b25cb518f70a4d32c
zhangheng <zhangheng2683@gmail.com>
2018-06-16 17:56:47 +0800

    zh

--- common/src/test/java/com/zh/JavaEnvTest.java
+++ common/src/test/java/com/zh/JavaEnvTest.java
@@ -1,16 +1,27 @@
 package com.zh;
 
 
-import java.io.File;
+import com.zh.log4j.Log4j;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class JavaEnvTest {
 
-    public static void main(String[] args) {
-        System.out.println(File.pathSeparator);
-        System.out.println(File.separator);
-        System.out.println(File.pathSeparatorChar);
-        System.out.println(File.separatorChar);
-        String s = "sadfasdf;asdfasd;asdf";
-        System.out.println(s.substring(0, s.lastIndexOf(";")));
+    public static void main(String[] args) throws InterruptedException {
+        int core_num = Runtime.getRuntime().availableProcessors();
+        ThreadPoolExecutor tpe = new ThreadPoolExecutor(core_num,
+                core_num * 2,
+                60,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(50));
+        for (int i = 0; i < 50; i++) {
+            tpe.execute(() -> {
+                Log4j.init(JavaEnvTest.class);
+            });
+        }
+        TimeUnit.SECONDS.sleep(30);
+        Log4j.info("初始化结束了...");
     }
 }
