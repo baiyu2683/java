68855c2ab1989edab5c0583367bc282115cd6d15
zhangheng <zhangheng2683@gmail.com>
2018-08-19 22:49:20 +0800

    zh

---
+++ algorithms/src/main/java/com/zh/chapter1/chapter13/exercise/Buffer.java
@@ -0,0 +1,57 @@
+package com.zh.chapter1.chapter13.exercise;
+
+/**
+ * 文本编辑器缓冲区
+ * 提示: 使用两个栈
+ * @author zh
+ * 2018年8月19日
+ */
+public class Buffer {
+	
+	/**
+	 * 创建一个新的缓冲区
+	 */
+	public Buffer() {
+		
+	}
+	
+	/**
+	 * 插入一个字符
+	 * @param c
+	 */
+	public void insert(char c) {
+		
+	}
+	
+	/**
+	 * 删除一个字符
+	 * @return
+	 */
+	public char delete() {
+		return 'c';
+	}
+	
+	/**
+	 * 将光标向左移动k个位置
+	 * @param k
+	 */
+	public void left(int k) {
+		
+	}
+	
+	/**
+	 * 将光标向右移动k个位置
+	 * @param k
+	 */
+	public void right(int k) {
+		
+	}
+	
+	/**
+	 * 缓冲区中的字符数量
+	 * @return
+	 */
+	public int size() {
+		return 0;
+	}
+}
