56031586cda20f174b413c36718a93eb52fda00f
zhangheng <zhangheng2683@gmail.com>
2018-08-27 18:54:11 +0800

    zhj

--- common/src/main/java/com/zh/util/SocketUtil.java
+++ common/src/main/java/com/zh/util/SocketUtil.java
@@ -11,6 +11,19 @@ import java.util.concurrent.TimeUnit;
 public class SocketUtil {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress(1234));
+        OutputStream os = socket.getOutputStream();
+        for (int count = 0 ; count < 10 ; count++) {
+            for (int i = 0; i < 4; i++) {
+                os.write(String.valueOf(i).getBytes());
+            }
+            os.flush();
+        }
+        TimeUnit.SECONDS.sleep(100);
+    }
+
+    public static void main1(String[] args) throws IOException, InterruptedException {
         try {
             SocketChannel sc = SocketChannel.open();
             sc.connect(new InetSocketAddress("127.0.0.1", 1234));
