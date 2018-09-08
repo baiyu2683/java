b6d8392d70294e88193473808d924a934f95b528
zhangheng <zhangheng2683@gmail.com>
2018-09-08 21:50:08 +0800

    zh

---
+++ algorithms/src/main/java/com/zh/chapter1/chapter15/UF.java
@@ -0,0 +1,27 @@
+package com.zh.chapter1.chapter15;
+
+/**
+ * union-find算法
+ */
+public class UF {
+
+    public UF(int N) {
+
+    }
+
+    public void union(int p, int q) {
+
+    }
+
+    public int find(int p) {
+
+    }
+
+    public boolean connected(int p, int q) {
+
+    }
+
+    public int count() {
+
+    }
+}
