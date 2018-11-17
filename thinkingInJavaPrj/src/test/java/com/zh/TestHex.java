209768c8432cb9d20beed8e0384ad0f2e549dd85
zhangheng <zhangheng2683@gmail.com>
2018-11-17 16:10:05 +0800

    hex测试

---
+++ thinkingInJavaPrj/src/test/java/com/zh/TestHex.java
@@ -0,0 +1,11 @@
+package com.zh;
+
+/**
+ * @Author zh2683
+ */
+public class TestHex {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.valueOf("1000"));
+    }
+}
