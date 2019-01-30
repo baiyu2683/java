f90741eaed279e94cc176b72acdb68da6a7affa3
zhangheng <zhangheng2683@gmail.com>
2019-01-30 18:22:17 +0800

    并发锁

--- assemblyTest/src/main/java/com/zh/zk/ZookeeperTest.java
+++ assemblyTest/src/main/java/com/zh/zk/ZookeeperTest.java
@@ -12,35 +12,34 @@ import java.util.concurrent.TimeUnit;
  */
 public class ZookeeperTest implements Watcher {
 
+    private static final String local_dir = "/lock";
+
     @Override
     public void process(WatchedEvent watchedEvent) {
         System.out.println(watchedEvent);
     }
 
     public static void main(String[] args) throws InterruptedException{
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
-        for (int i = 0 ; i < 10 ; i++) {
+        int count = Runtime.getRuntime().availableProcessors();
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(count);
+        for (int i = 0 ; i < count ; i++) {
             new Thread(() -> {
                 ZooKeeper zooKeeper = null;
                 try {
                     cyclicBarrier.await();
                     zooKeeper = new ZooKeeper("127.0.0.1:2181", 1000, new ZookeeperTest());
-                    String path = zooKeeper.create("/home", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+                    // 获得锁
+                    String path = zooKeeper.create(local_dir, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
                     System.out.println("成功-" + Thread.currentThread().getName());
+                    TimeUnit.SECONDS.sleep(5);
+                    // 释放锁
+                    zooKeeper.delete(local_dir, -1);
                 } catch (KeeperException | IOException | InterruptedException | BrokenBarrierException e) {
                     System.out.println("失败-" + Thread.currentThread().getName() + " " + e.getMessage());
-                } finally {
-                    if (zooKeeper != null) {
-                        try {
-                            zooKeeper.close();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
                 }
             }).start();
         }
-        TimeUnit.SECONDS.sleep(30);
+        TimeUnit.SECONDS.sleep(20);
     }
 
 }
