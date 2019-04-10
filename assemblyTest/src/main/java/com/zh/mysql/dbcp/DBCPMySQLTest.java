8ce1e1839c835a886d067112380f788c537ec3bc
zhangheng2683 <zhangheng2683@gmail.com>
2019-04-10 18:10:47 +0800

    dbcp测试mysql和oracle存储过程调用

--- assemblyTest/src/main/java/com/zh/mysql/dbcp/DBCPMySQLTest.java
+++ assemblyTest/src/main/java/com/zh/mysql/dbcp/DBCPMySQLTest.java
@@ -1,9 +1,12 @@
 package com.zh.mysql.dbcp;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
+import oracle.jdbc.OracleResultSet;
+import oracle.jdbc.OracleTypes;
+import oracle.jdbc.oracore.OracleType;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.Test;
 
 /**
  * dbcp连接mysql8
@@ -13,15 +16,51 @@ import org.apache.commons.dbcp.BasicDataSource;
  */
 public class DBCPMySQLTest {
 
-    public static void main(String[] args) throws SQLException {
+    @Test
+    public void testmyql() throws SQLException {
         BasicDataSource bds = new BasicDataSource();
         bds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        bds.setUrl("jdbc:mysql://localhost:3306/test?allowPublicKeyRetrieval=true&serverTimezone=Asia/Shanghai&useSSL=false&useUnicode=True&characterEncoding=utf-8");
-        bds.setUrl("jdbc:mysql://127.0.0.1:3306/test?user=root&password=123456&useUnicode=true&characterEncoding=gb18030&autoReconnect=true&failOverReadOnly=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Shanghai&useSSL=false");
-//        bds.setUsername("root");
-//        bds.setPassword("123456");
+        bds.setUrl("jdbc:mysql://127.0.0.1:3306/zh?user=root&password=root&useUnicode=true&characterEncoding=gb18030&autoReconnect=true&failOverReadOnly=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Shanghai&useSSL=false");
+
         Connection conn = bds.getConnection();
-        System.out.println(conn.createStatement().execute("select 1"));
+
+        CallableStatement prepareCall = conn.prepareCall("{call first(?, ?)}");
+        prepareCall.setString("name", "dld31");
+        prepareCall.registerOutParameter("tmpname", Types.VARCHAR);
+
+        ResultSet resultSet = prepareCall.executeQuery();
+        System.out.println(prepareCall.getString("tmpname"));
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("id"));
+        }
+        conn.close();
+    }
+
+    @Test
+    public void testOracle() throws SQLException {
+        BasicDataSource bds = new BasicDataSource();
+
+        bds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        bds.setUrl("jdbc:oracle:thin:@129.204.212.96:1521:helowin");
+        bds.setUsername("zh");
+        bds.setPassword("1234qwer");
+
+        Connection conn = bds.getConnection();
+
+        CallableStatement prepareCall = conn.prepareCall("{call \"first\"(?, ?, ?)}");
+        prepareCall.setString("name", "dld3");
+        prepareCall.registerOutParameter("outname", Types.VARCHAR);
+        prepareCall.registerOutParameter("cur_arg", OracleTypes.CURSOR);
+
+        prepareCall.executeQuery();
+        System.out.println(prepareCall.getString("outname"));
+
+        ResultSet resultSet = (ResultSet) prepareCall.getObject("cur_arg");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("id"));
+            System.out.println(resultSet.getString("nickname"));
+        }
         conn.close();
     }
 }
