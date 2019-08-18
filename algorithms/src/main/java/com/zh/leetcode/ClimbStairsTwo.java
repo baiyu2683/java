331fb72f34f445119da69f64ddb0ec570be2a059
zhangheng2683 <zhangheng2683@gmail.com>
2019-08-18 16:13:07 +0800

    1,上阶梯 2,树是否是镜像的

---
+++ algorithms/src/main/java/com/zh/leetcode/ClimbStairsTwo.java
@@ -0,0 +1,58 @@
+package com.zh.leetcode;
+
+/**
+ * 简单
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ *
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/climbing-stairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 递归方法中由于递归过程中会有很多重复计算，可进行优化
+ *  第一种将递归树进行减支操作，即将中间结果缓存起来
+ *  第二种像下面逐步计算
+ */
+public class ClimbStairsTwo {
+
+    public static void main(String[] args) {
+        ClimbStairsTwo climbStairs = new ClimbStairsTwo();
+        System.out.println(climbStairs.climbStairs(44));
+    }
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] results = new int[n + 1];
+        results[1] = 1;
+        results[2] = 2;
+        for (int i = 3 ; i <= n ; i++) {
+            results[i] = results[i - 1] + results[i - 2];
+        }
+        return results[n];
+    }
+}
---
+++ algorithms/src/main/java/com/zh/leetcode/IsSymmetric.java
@@ -0,0 +1,49 @@
+package com.zh.leetcode;
+
+/**
+ * 简单
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * 说明:
+ *
+ * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsSymmetric {
+
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val == right.val) {
+            return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+        }
+        return false;
+    }
+}
