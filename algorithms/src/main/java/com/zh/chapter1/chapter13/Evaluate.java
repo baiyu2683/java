f0738e2c7b92ebf324d0003c5c70cb9666cf75e8
zhangheng2683 <zhangheng2683@gmail.com>
2020-01-18 22:51:22 +0800

    表达式解析增加not运算符

--- algorithms/src/main/java/com/zh/chapter1/chapter13/Evaluate.java
+++ algorithms/src/main/java/com/zh/chapter1/chapter13/Evaluate.java
@@ -1,5 +1,8 @@
 package com.zh.chapter1.chapter13;
 
+import lombok.Data;
+import lombok.NonNull;
+
 import java.util.*;
 
 /**
@@ -9,28 +12,53 @@ import java.util.*;
 public class Evaluate {
 
     public static void main(String[] args) {
-        String expression = "$1$ and $1,2,3$";
-//        expression = "$1,2,4$ and ($4,5,6$ or $7,8,9$)";
-//        expression = "$1,2,3$ and ($4,2,6$ or $1,7,8,9$) and $1,2,6$";
-        List<Integer> list = evaluateExpress(expression);
-        System.out.println(Arrays.toString(list.toArray()));
+        String expression = "($1$ and not $2,3,4$) or not $2,3$";
+        Value result = evaluateExpress(expression);
+        System.out.println(result.toString());
     }
 
-    private static List<Integer> evaluateExpress(String expression) {
+    private static Value evaluateExpress(String expression) {
         String[] subStr = expression.split("\\s*");
+        return evaluateExpress(subStr);
+    }
 
+    private static Value evaluateExpress(String[] expression) {
         Deque<String> ops = new LinkedList<>();
-        Deque<List<Integer>> vals = new LinkedList<>();
+        Deque<Value> vals = new LinkedList<>();
         // 单个数字
         StringBuilder data = new StringBuilder();
         // 一个$$中间所有的数字
         List<Integer> dataList = new ArrayList<>();
         boolean isValue = false;
-        for (int i = 0 ; i < subStr.length ; i++) {
-            String sub = subStr[i];
-            if (sub.equals("$")) {
+        boolean isNot = false;
+        for (int i = 0 ; i < expression.length ; i++) {
+            String sub = expression[i];
+            if (sub.equals("(") && isNot) {
+                //  优先计算not(expression)格式的子表达式
+                Deque<String> subOps = new LinkedList<>();
+                subOps.push("(");
+                List<String> notExpress = new ArrayList<>();
+                int j = i + 1;
+                for (; j < expression.length ; j++) {
+                    String content = expression[j];
+                    if (content.equals(")")) {
+                        subOps.pop();
+                    } else {
+                        notExpress.add(content);
+                    }
+                    if (subOps.isEmpty()) {
+                        break;
+                    }
+                }
+                Value value = evaluateExpress(notExpress.toArray(new String[0]));
+                if (!value.isNot()) {
+                    value.setNot(true);
+                }
+                isNot = false;
+                vals.push(value);
+                i = j + 1;
+            } else if (sub.equals("$")) {
                 if (isValue) {
-                    // 第二个$
 
                     //最后一个数字
                     if (data.length() > 0) {
@@ -38,11 +66,19 @@ public class Evaluate {
                         data.setLength(0);
                     }
 
+                    // 第二个$
                     List<Integer> clone = new ArrayList<>();
                     for (Integer d : dataList) {
                         clone.add(d);
                     }
-                    vals.push(clone);
+                    Value value = new Value(clone);
+                    if (isNot) {
+                        // not $1,2,3$ 格式
+                        value.setNot(isNot);
+                        isNot = false;
+                    }
+
+                    vals.push(value);
                     dataList.clear();
                 }
                 isValue = !isValue;
@@ -62,44 +98,84 @@ public class Evaluate {
             } else if (sub.equals("o")) {
                 ops.push("or");
                 i += 1;
+            } else if (sub.equals("n")) {
+//                ops.push("not");
+                i += 2;
+                isNot = true;
             } else if (sub.equals(")")) {
-                // 操作符出栈
-                String op = ops.pop();
-                // 第一个操作数出栈
-                List<Integer> v = vals.pop();
-                Set<Integer> set = new HashSet<>(v);
-                if (op.equals("and")) {
-                    // 求交集
-                    set.retainAll(vals.pop());
-                    v = new ArrayList<>(set);
-                }else if (op.equals("or")) {
-                    // 求并集
-                    set.addAll(vals.pop());
-                    v = new ArrayList<>(set);
-                }
-                // 值压栈
-                vals.push(v);
+                evaluate(ops, vals);
             }
         }
         while (ops.size() > 0) {
-            // 操作符出栈
-            String op = ops.pop();
-            // 第一个操作数出栈
-            List<Integer> v = vals.pop();
-            Set<Integer> set = new HashSet<>(v);
-            if (op.equals("and")) {
-                // 求交集
-                set.retainAll(vals.pop());
-                v = new ArrayList<>(set);
-            }else if (op.equals("or")) {
-                // 求并集
-                set.addAll(vals.pop());
-                v = new ArrayList<>(set);
-            }
-            // 值压栈
-            vals.push(v);
+            evaluate(ops, vals);
         }
         return vals.pop();
+    }
+
+    private static void evaluate(Deque<String> ops, Deque<Value> vals) {
+        // 操作符出栈
+        String op = ops.pop();
+        Value result = null;
+        // 第一个操作数出栈
+        Value firstValue = vals.pop();
+        if (op.equals("and")) {
+
+            Set<Integer> first = new HashSet<>(firstValue.getValues());
+            boolean firstNot = firstValue.isNot();
+
+            Value secondValue = vals.pop();
+            Set<Integer> second = new HashSet<>(secondValue.getValues());
+            boolean secondNot = secondValue.isNot();
+
+            if (firstNot && !secondNot) {
+                second.remove(first);
+                result = new Value(new ArrayList<>(second));
+            } else if (!firstNot && secondNot) {
+                first.remove(second);
+                result = new Value(new ArrayList<>(first));
+            } else if (firstNot && secondNot) {
+                first.addAll(second);
+                result = new Value(new ArrayList<>(first));
+                result.setNot(true);
+            } else {
+                first.retainAll(second);
+                result = new Value(new ArrayList<>(first));
+            }
+        } else if (op.equals("or")) {
+            Set<Integer> first = new HashSet<>(firstValue.getValues());
+            boolean firstNot = firstValue.isNot();
+
+            Value secondValue = vals.pop();
+            Set<Integer> second = new HashSet<>(secondValue.getValues());
+            boolean secondNot = secondValue.isNot();
+
+            if (firstNot && !secondNot) {
+                first.remove(second);
+                result = new Value(new ArrayList<>(first));
+                result.setNot(true);
+            } else if (!firstNot && secondNot) {
+                second.remove(first);
+                result = new Value(new ArrayList<>(second));
+                result.setNot(true);
+            } else if (firstNot && secondNot) {
+                first.retainAll(second);
+                result = new Value(new ArrayList<>(first));
+                result.setNot(true);
+            } else {
+                first.addAll(second);
+                result = new Value(new ArrayList<>(first));
+            }
+        }
+        // 值压栈
+        vals.push(result);
+    }
+
+    @Data
+    private static class Value {
+        //是否包含values
+        private boolean not;
+        @NonNull
+        private List<Integer> values;
     }
 
 }
