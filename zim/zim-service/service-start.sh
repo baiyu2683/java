5da0c6543e506a443bfac9c925b6102b43b77c06
zhangheng2683 <zhangheng2683@gmail.com>
2020-04-07 21:32:50 +0800

    移除zim模块作为单独项目

--- zim/zim-service/service-start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ip=$(ifconfig | grep -A 1 'eth0' | grep 'inet' | sed 's/^.*inet //g' | cut -d ' ' -f 1 | sed 's/^addr://g')
-
-nohup java -Dzim.serverIp=$ip -jar ./lib/zim-service.jar >/dev/null 2>&1 &
