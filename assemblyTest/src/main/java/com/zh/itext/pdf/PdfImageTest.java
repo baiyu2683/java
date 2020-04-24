99d10aed7862e3fce8e9a59539a478e7d345979f
zhangheng2683 <zhangheng2683@gmail.com>
2020-04-24 18:36:15 +0800

    pdf图片模糊测试

--- assemblyTest/src/main/java/com/zh/itext/pdf/PdfImageTest.java
+++ assemblyTest/src/main/java/com/zh/itext/pdf/PdfImageTest.java
@@ -1,86 +1,97 @@
 package com.zh.itext.pdf;
 
-import com.aspose.words.HorizontalAlignment;
 import com.itextpdf.text.*;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.*;
-import com.itextpdf.tool.xml.XMLWorkerFontProvider;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 public class PdfImageTest {
 
     public static void main(String[] args) throws IOException, DocumentException {
         Document document = new Document();
-
-        FontFactoryImp fp = new XMLWorkerFontProvider();
-        fp.register("D:\\fonts");
-
-        PdfWriter pdfWriter= PdfWriter.getInstance(document, new FileOutputStream(new File("e:/test.pdf")));
-        document.setPageSize(PageSize.A10);
+        PdfWriter pdfWriter= PdfWriter.getInstance(document, new FileOutputStream(new File("C:\\Users\\Administrator\\Desktop\\test.pdf")));
         document.setPageSize(PageSize.A4);
-        document.setMargins(51f, 51f, 53f, 53f);
+        document.setMargins(48f, 48f, 72f, 72f);
         document.open();
 
-        PdfPCell emptyCell = new PdfPCell(new Phrase(" "));
-        emptyCell.setPadding(0);
+        float pageBottomMargin = 72f;
+        float pageLeftMargin = 48f;
 
-        PdfPTable table = new PdfPTable(1);
-        table.setHorizontalAlignment(HorizontalAlignment.LEFT);
-        table.setTotalWidth(new float[]{100});
-        table.getDefaultCell().setPadding(1);
-        table.setLockedWidth(true);
-        Font font = fp.getFont("微软雅黑", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        font.setSize(9);
-        for (int i = 0 ; i < 100 ; i++) {
-            String text = i + "";
-            String tmpText = getDispText(text);
-            Phrase phrase = new Phrase(tmpText, font);
-            PdfPCell cell = new PdfPCell(phrase);
-            table.addCell(cell);
-            table.completeRow();
+        BufferedImage bufferedImage = ImageIO.read(new File("C:\\Users\\Administrator\\Desktop\\htw_2020_02_14_08_57_23.png"));
+        int imageWidth = bufferedImage.getWidth();
+        int imageHeight = bufferedImage.getHeight();
+
+        float pageHeight = document.getPageSize().getHeight();
+        float pageWidth = document.getPageSize().getWidth();
+
+        int hCount = (int) (imageHeight / pageHeight + 1);
+        for (int count = 1 ; count <= hCount ; count++) {
+            int dx1 = 0;
+            int dy1 = (int) (pageHeight * (count - 1));
+            int dx2 = (int) pageWidth;
+            int dy2 = (int) (dy1 + pageHeight);
+            if (dy2 > imageHeight) {
+                dy2 = (int) imageHeight;
+            }
+            Image image = imageCut(bufferedImage,
+                    dx1, dy1, dx2, dy2,
+                    dx2 - dx1,
+                    dy2 - dy1);
+
+            int positionY = px2Pong((int)pageBottomMargin);
+            if (count == hCount) {
+                positionY = px2Pong((int) (pageHeight - (dy2 - dy1) + pageBottomMargin));
+            }
+            image.setAbsolutePosition(px2Pong((int)pageLeftMargin), positionY);
+            pdfWriter.getDirectContent().addImage(image);
+            if (count != hCount) {
+                document.newPage();
+            }
         }
-        table.setTableEvent(new TableSplitEvent());
-        document.add(table);
         document.close();
         pdfWriter.flush();
     }
 
-    /**
-     * 添加图片
-     * @param writer
-     * @param document
-     */
-    private static void addImage(PdfWriter writer, Document document) {
-        try {
-            Image img = Image.getInstance("D:\\07docx.png");
-            img.setAbsolutePosition(100, 100);
-            document.add(img);
-///            PdfContentByte cont = writer.getDirectContentUnder();
-///            cont.addImage(img);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private static Image imageCut(BufferedImage srcBufferedImage,
+                                  int dx1, int dy1, int dx2, int dy2, int destImageWidth,
+                                  int destImageHeight) throws IOException, BadElementException {
+        int width = srcBufferedImage.getWidth();
+        int height = srcBufferedImage.getHeight();
+        BufferedImage bufferedImage = new BufferedImage(destImageWidth, destImageHeight, srcBufferedImage.getType());
+        Graphics2D graphics2D = bufferedImage.createGraphics();
+        graphics2D.setComposite(AlphaComposite.Clear);
+        graphics2D.fillRect(0, 0, destImageWidth, destImageHeight);
+        graphics2D.setComposite(AlphaComposite.SrcOver);
+        graphics2D.fillRect(0, 0, destImageWidth, destImageHeight);
+//        graphics2D.drawImage(srcBufferedImage,
+//                0, 0, destImageWidth, destImageHeight,
+//                dx1, dy1, dx2, dy2,
+//                null);
+        graphics2D.drawImage(srcBufferedImage.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH),
+                0, 0, destImageWidth, destImageHeight,
+                dx1, dy1, dx2, dy2,
+                null);
+        bufferedImage.flush();
+        graphics2D.dispose();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", outputStream);
+        byte[] newImageData = outputStream.toByteArray();
+        Image newImage = Image.getInstance(newImageData);
+        newImage.scaleAbsolute(px2Pong(destImageWidth), px2Pong(destImageHeight));
+        return newImage;
     }
 
-    /**
-     * @FIXME
-     * java8 有sun.font.FontDesignMetrics
-     * java11 去掉了...
-     * @param text
-     * @return
-     */
-    private static String getDispText(String text) {
-        String tmpText = text;
-//        java.awt.FontMetrics fm = sun.font.FontDesignMetrics.getMetrics(new java.awt.Font("微软雅黑", java.awt.Font.PLAIN, 9));
-//        int w = fm.stringWidth(text);
-//        while (w > 120) {
-//            tmpText = tmpText.substring(0, tmpText.length() - 1);
-//            fm = sun.font.FontDesignMetrics.getMetrics(new java.awt.Font("微软雅黑", java.awt.Font.PLAIN, 9));
-//            w = fm.stringWidth(tmpText);
-//        }
-//        tmpText = tmpText.substring(0, tmpText.length() - 3) + "...";
-        return tmpText;
+    private static int px2Pong(int px) {
+        return px * 3 / 4;
     }
 }
