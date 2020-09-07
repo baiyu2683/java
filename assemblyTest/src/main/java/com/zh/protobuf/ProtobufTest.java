97a1a76a2ed17d37c4e517288cbf8334a8f86d66
zhangheng2683 <zhangheng2683@gmail.com>
2020-09-07 10:31:14 +0800

    测试下序列化反序列化大小和事件

--- assemblyTest/src/main/java/com/zh/protobuf/ProtobufTest.java
+++ assemblyTest/src/main/java/com/zh/protobuf/ProtobufTest.java
@@ -9,7 +9,7 @@ import io.protostuff.Schema;
 import io.protostuff.runtime.RuntimeSchema;
 import org.junit.Test;
 
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 
 /**
  * 需要protobuf插件编译生成相关类
@@ -57,6 +57,8 @@ public class ProtobufTest {
         System.out.println(user.toByteArray().length);
     }
 
+    private static int counter = 1000000;
+
     @Test
     public void test2() {
 
@@ -72,12 +74,60 @@ public class ProtobufTest {
                         .setPassword("20").build())
                 .build();
 
-        Schema<DataWrapper> schema = RuntimeSchema.getSchema(DataWrapper.class);
-        byte[] data = ProtobufIOUtil.toByteArray(wrapper, schema, LinkedBuffer.allocate());
+        long start = System.currentTimeMillis();
+        for (int i = 0 ; i < counter ; i++) {
+            Schema<DataWrapper> schema = RuntimeSchema.getSchema(DataWrapper.class);
+            byte[] data = ProtobufIOUtil.toByteArray(wrapper, schema, LinkedBuffer.allocate());
+            DataWrapper wrapper1 = DataWrapper.newBuilder().build();
+            ProtobufIOUtil.mergeFrom(data, wrapper1, schema);
+//            System.out.println(wrapper1.toString());
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
 
-        DataWrapper wrapper1 = DataWrapper.newBuilder().build();
-        ProtobufIOUtil.mergeFrom(data, wrapper1, schema);
-        System.out.println(wrapper1.toString());
+    @Test
+    public void test3() {
+        LoginRequestDTO1 loginRequestDTO1 = new LoginRequestDTO1();
+        loginRequestDTO1.setCode("10010");
+        loginRequestDTO1.setPassword("20");
+
+        long start = System.currentTimeMillis();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        for (int i = 0 ; i < counter ; i++) {
+            byteArrayOutputStream.reset();
+            try {
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+                objectOutputStream.writeObject(loginRequestDTO1);
+                objectOutputStream.close();
+                ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()));
+                objectInputStream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    public static class LoginRequestDTO1 implements Serializable {
+        private String code;
+
+        private String password;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
     }
 
 }
