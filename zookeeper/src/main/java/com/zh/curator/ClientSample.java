238ce9e65a4373f9caa9260ca951846a59da8c40
cellophane <zhangheng2683@gmail.com>
2021-10-09 11:01:25 +0800

    curator设置acl

--- zookeeper/src/main/java/com/zh/curator/ClientSample.java
+++ zookeeper/src/main/java/com/zh/curator/ClientSample.java
@@ -1,10 +1,13 @@
 package com.zh.curator;
 
+import com.google.common.collect.Lists;
 import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.AuthInfo;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
 public class ClientSample {
@@ -13,6 +16,7 @@ public class ClientSample {
     public static RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
     public static CuratorFramework client = CuratorFrameworkFactory
             .builder()
+//            .authorization("digest", "zh2683:root".getBytes(StandardCharsets.UTF_8))
             .retryPolicy(retryPolicy)
             .connectString(connectString)
             .sessionTimeoutMs(6000)   // 设置超时时间，如果超时curator会自动重连
@@ -21,6 +25,17 @@ public class ClientSample {
     public static CuratorFramework client() {
         return CuratorFrameworkFactory
                 .builder()
+//                .authorization("auth", "zh2683:root".getBytes(StandardCharsets.UTF_8))
+                .retryPolicy(retryPolicy)
+                .connectString(connectString)
+                .sessionTimeoutMs(6000)   // 设置超时时间，如果超时curator会自动重连
+                .build();
+    }
+
+    public static CuratorFramework client(AuthInfo authInfo) {
+        return CuratorFrameworkFactory
+                .builder()
+                .authorization(Lists.newArrayList(authInfo))
                 .retryPolicy(retryPolicy)
                 .connectString(connectString)
                 .sessionTimeoutMs(6000)   // 设置超时时间，如果超时curator会自动重连
